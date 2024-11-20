bda4e91f648761bbb07729cd33ac89715ebc6ea7
Amy Chih <amy83429@gmail.com>
2024-11-20 23:11:54 +0800

    this version from Kevin

---
+++ Models/Post.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Linq;
+
+namespace NETELLUS.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // 一對多關係, 每篇文章有多個評論
+        public List<Comment> Comments { get; set; } = [];
+
+        // 文章的點讚列表
+        public List<Like> Likes { get; set; } = [];
+    }
+}
